45d13a4982cf4f9892c3964753a2786891fb2dd4
Mayank Agnihotri <mayankagnihotri7@gmail.com>
2020-06-15 22:56:45 +0530

    add css

--- src/components/userProfile.jsx
+++ src/components/userProfile.jsx
@@ -1,17 +1,22 @@
 import React from "react";
+import "./userProfile.component.css";
 
 const UserProfile = (props) => {
   console.log(props, "here we hav");
   return (
-    <div>
-      <img src={props.details.avatar_url} alt="" />
-      <h1>{props.details.login}</h1>
-      <h2>{props.details.bio}</h2>
-      <a href={props.details.blog}>{props.details.blog}</a>
-      <h2>{props.details.followers}</h2>
-      <label>Following:</label>
-      <h2>{props.details.following}</h2>
-      <a href={props.details.url}>{props.details.url}</a>
+    <div className="container grid">
+      <div>
+        <img src={props.details.avatar_url} alt="" />
+      </div>
+      <div>
+        <h1>{props.details.login}</h1>
+        <h2>{props.details.bio}</h2>
+        <a href={props.details.blog}>{props.details.blog}</a>
+        <h2>{props.details.followers}</h2>
+        <label>Following:</label>
+        <h2>{props.details.following}</h2>
+        <a href={props.details.url}>{props.details.url}</a>
+      </div>
     </div>
   );
 };
